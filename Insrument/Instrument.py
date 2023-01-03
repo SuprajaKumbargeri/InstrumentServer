e874916297b1ff45452996456d02764620f07dff
tstar98 <43146669+tstar98@users.noreply.github.com>
2023-01-03 10:08:07 -0700

    Implemented basic methods for instrument class

--- Insrument/Instrument.py
+++ Insrument/Instrument.py
@@ -11,22 +11,22 @@ class BaseInstrument:
     def write(self, msg):
         raise NotImplementedError()
 
-    def read(self, msg):
+    def read(self):
         raise NotImplementedError()
 
-    def read_values(self, msg):
+    def read_values(self):
         raise NotImplementedError()
 
-    def ask_for_values(self, msg):
+    def ask_for_values(self, format):
         raise NotImplementedError()
 
-    def clear(self, msg):
+    def clear(self):
         raise NotImplementedError()
 
-    def trigger(self, msg):
+    def trigger(self):
         raise NotImplementedError()
 
-    def read_raw(self, msg):
+    def read_raw(self):
         raise NotImplementedError()
 
     @property
@@ -38,7 +38,6 @@ class Instrument(BaseInstrument):
     def __int__(self, driver, instrument_resource, name=None):
         # parse driver
         self._config = {}
-
         self._rm, self._instrument = self._initialize_resource_manager(instrument_resource)
 
         if name is None:
@@ -47,6 +46,7 @@ class Instrument(BaseInstrument):
             super().__int__(name)
 
     def _initialize_resource_manager(self, instrument_resource):
+        # string passed through in VISA form
         if isinstance(instrument_resource, str):
             resource_manager = ResourceManager()
             instrument = resource_manager.get_instrument(instrument_resource)
@@ -63,25 +63,25 @@ class Instrument(BaseInstrument):
         return self._instrument.ask(msg)
 
     def write(self, msg):
-        pass
+        self._instrument.write(msg)
 
-    def read(self, msg):
-        pass
+    def read(self):
+        return self._instrument.read()
 
-    def read_values(self, msg):
-        pass
+    def read_values(self, format):
+        return self._instrument.read_values(format)
 
     def ask_for_values(self, msg):
         pass
 
-    def clear(self, msg):
-        pass
+    def clear(self):
+        self._instrument.clear()
 
-    def trigger(self, msg):
-        pass
+    def trigger(self, ):
+        self._instrument.trigger()
 
-    def read_raw(self, msg):
-        pass
+    def read_raw(self):
+        self._instrument.read_raw()
 
     @property
     def name(self):
@@ -105,7 +105,51 @@ class Instrument(BaseInstrument):
 
     @timeout.setter
     def timeout(self, value):
-        self._instr.timeout = value
+        self._instrument.timeout = value
+
+    @property
+    def delay(self):
+        return self._instrument.delay
+
+    @delay.setter
+    def delay(self, value):
+        self._instrument.delay = value
+
+    def get_value(self, quantity):
+        return self._instrument.ask(self.quantities[quantity]['get_cmd'])
+
+    def set_value(self, quantity, value):
+        lower_lim = self.quantities[quantity]['low_lim']
+        upper_lim = self.quantities[quantity]['high_lim']
+
+        if not lower_lim == '-INF' and value < float(lower_lim):
+            raise ValueError(f"{value} is lower than {quantity}'s lower limit of {lower_lim}.")
+        if not upper_lim == '-INF' and value < float(upper_lim):
+            raise ValueError(f"{value} is higher than {quantity}'s upper limit of {upper_lim}.")
+
+        cmd = self.quantities[quantity]['set_cmd']
+        if "<*>" in cmd:
+            cmd.replace("<*>", value)
+        else:
+            cmd += value
+
+        self._instrument.write()
+
+    def get_state(self, quantity):
+        if self.quantities[quantity]['state_quant']:
+            return self.quantities[quantity]['state_quant']
+        return None
+
+    def set_state(self, quantity, state):
+        if state not in self.quantities[quantity]['state_values']:
+            raise ValueError(f'{state} is not a valid state for {quantity}.')
+        self.quantities[quantity]['state_quant'] = state
+
+    def __getitem__(self, quantity):
+        return self.get_value(quantity)
+
+    def __setitem__(self, item, value):
+
 
     def __exit__(self, exc_type, exc_val, exc_tb):
         self.close()
